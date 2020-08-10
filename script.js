5c79b8a63dd9fa528e58ae5df8611bb0c9500556
matan gadasi <matan230g@gmail.com>
2020-08-10 20:15:13 +0300

    add section my work

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const navbarSlide =  () =>{
     burger.addEventListener('click', () =>
     {
         nav.classList.toggle('nav-active');
-
     });
     
+}
+const skillBar = () => {
+    const htmlskill = document.querySelector('.html');
+  
+    htmlskill.addEventListener('click',()=>{
+        console.log("trigger work");
+        htmlskill.classList.add('zero');
+    });
 
 }
 
     
 
 navbarSlide();
-
+skillBar();
    
